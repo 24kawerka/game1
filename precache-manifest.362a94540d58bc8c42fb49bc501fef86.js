149868e7657b6459752cd1464e0b030f4d32d269
mFarbum <mfarbun@huspi.com>
2024-02-18 14:25:34 +0200

    Updates

---
+++ precache-manifest.362a94540d58bc8c42fb49bc501fef86.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4e52f8e65a60e2add9bb77fe1ae07a",
+    "url": "/game1/index.html"
+  },
+  {
+    "revision": "69ddf27feba8edc9c86e",
+    "url": "/game1/static/css/main.e2a6e733.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/game1/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/game1/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "69ddf27feba8edc9c86e",
+    "url": "/game1/static/js/main.85583cf2.chunk.js"
+  },
+  {
+    "revision": "18f5084b16db540475ce",
+    "url": "/game1/static/js/runtime~main.f9ecb2f6.js"
+  },
+  {
+    "revision": "0d4176691397b22d74dab6ea7215da26",
+    "url": "/game1/static/media/background.0d417669.jpg"
+  },
+  {
+    "revision": "bc5ad905075b8c205b8e7abc8ea69ae3",
+    "url": "/game1/static/media/health.bc5ad905.png"
+  },
+  {
+    "revision": "741d72761a54becae9435a536dd62b17",
+    "url": "/game1/static/media/sword.741d7276.jpg"
+  }
+]);
