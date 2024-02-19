9e6056d14faa695484c8ede47456328e9f768e3a
mFarbum <mfarbun@huspi.com>
2024-02-19 23:51:21 +0200

    add hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import StartMenu from './pages/StartMenu';
 import GamePage from './pages/GamePage';
+
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.REACT_APP_BASE_PATH ? '' : '/game1'}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<StartMenu />} />
         <Route path="game" element={<GamePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
